76d03f6f06c1ad1a54bd288f63e97d7a08a706f2
Usman Tanveer <usman.tanveer@emumba.com>
2024-12-26 18:10:43 +0500

    Write python code for nginx app

---
+++ nginx-deploy.py
@@ -0,0 +1,37 @@
+from ray import serve
+from ray.serve.handle import DeploymentHandle
+
+# These imports are used only for type hints:
+from typing import Dict
+from starlette.requests import Request
+
+@serve.deployment
+class NgnixDeployment:
+    def __init__(self):
+    self.container_id = subprocess.check_output(
+	[
+	    "docker", "run", "-d", "--rm",
+	    "-p", "80:80",
+	    "nginx:laster"
+	]).decode("utf-8").strip()
+	print(f"Nginx container started with ID: {self.container_id}")
+    def __del__(self):
+	subprocess.run(["docker", "stop", self.container_id], check=True)
+	print(f"Nginx container stopped: {self.container_id}")
+
+    async def __call__(self, request):
+	return {"status": "Nginx container is running."}
+
+@serve.deployment
+class RootRouter:
+    def __init__(self):
+        self.nginx = NginxDeployment.bind()
+
+    async def __call__(self, request):
+	return {"message": "Welcome to the Ray Serve Nginx App!"}
+
+
+
+app = RootRouter.bind()
+
+deployment_graph = RootRouter.bind()
